3c08ffd5f9ea213654309e570e544dc9de01ddee
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2019-09-08 18:19:19 -0700

    get order process app into better shape

--- examples/order_process_app.rs
+++ examples/order_process_app.rs
@@ -2,21 +2,12 @@
 extern crate serde_derive;
 
 use atomic_counter::{AtomicCounter, RelaxedCounter};
-use futures::{Future, TryFuture, TryFutureExt, Stream, TryStream, TryStreamExt, StreamExt};
 use futures::prelude::*;
-use runtime::net::TcpListener;
 use std::sync::Arc;
-use std::thread::sleep;
 use std::time::Duration;
 use structopt::StructOpt;
-use zeebest::{Client, PublishMessage, WorkflowInstance, WorkflowVersion, ActivateJobs, ActivatedJob, PanicOption, ActivatedJobs, JobResult, CompleteJob, WorkerConfig};
-use std::pin::Pin;
-use std::io;
-use futures::compat::Stream01CompatExt;
-use std::panic::UnwindSafe;
-use zeebest::Error::FailJobError;
+use zeebest::{Client, PublishMessage, WorkflowInstance, WorkflowVersion, PanicOption, JobResult, WorkerConfig};
 use zeebest::worker_builder::WorkerBuilder;
-use futures::executor::block_on;
 use runtime::time::Interval;
 
 #[derive(StructOpt, Debug)]
@@ -63,110 +54,9 @@ struct Payment {
     pub order_value: f32,
 }
 
-//async fn sum_with_try_next(
-//    mut stream: Pin<&mut dyn Stream<Item = Result<i32, io::Error>>>,
-//) -> Result<i32, io::Error> {
-//    use futures::stream::TryStreamExt; // for `try_next`
-//    let mut sum = 0;
-//    while let Some(item) = stream.try_next().await? {
-//        sum += item;
-//    }
-//    Ok(sum)
-//}
-
-//async fn interval<Fut1,F>(client: Arc<Client>, f: Arc<F>, panic_option: PanicOption, mut interval_stream: impl Stream + Unpin) -> Result<i32, ()> where
-//    Fut1: Future<Output = JobResult> + Send + UnwindSafe + 'static,
-//    F: Fn(ActivatedJob) -> Fut1 + Send + Sync + 'static,
-//{
-////    let mut stream = Interval::new_interval(Duration::from_secs(1)).compat();
-//    while let Some(_) = interval_stream.next().await {
-//        let activate_jobs = ActivateJobs::new("the_worker", "the_job_type", 10000, 10);
-//        let mut activate_jobs_stream= client.activate_jobs(activate_jobs);
-//        while let Some(Ok(activated_jobs)) = activate_jobs_stream.next().await {
-//            for aj in activated_jobs.activated_jobs.into_iter() {
-//                let f = f.clone();
-//                let klient = client.clone();
-//                let job_key = aj.key;
-//                let retries = aj.retries;
-//                runtime::spawn(async move {
-//                    let r = match f(aj).catch_unwind().await {
-//                        Ok(r) => r,
-//                        Err(_) => {
-//                            match panic_option {
-//                                PanicOption::DoNothingOnPanic => {
-//                                    JobResult::Fail {
-//                                        error_message: Some("Job Handler Panicked.".to_string()),
-//                                    }
-//                                },
-//                                PanicOption::FailJobOnPanic => {
-//                                    JobResult::NoAction
-//                                }
-//                            }
-//                        },
-//                    };
-//                    match r {
-//                        JobResult::NoAction => {},
-//                        JobResult::Complete {variables} => {
-//                            let complete_job = CompleteJob { job_key, variables };
-//                            klient.complete_job(complete_job).await.unwrap();
-//                        },
-//                        JobResult::Fail {error_message} => {
-//                            klient.fail_job(job_key, retries - 1).await.unwrap();
-//                        }
-//                    }
-//                });
-//            };
-//        }
-//    }
-//    Ok(0)
-//}
-
-//            .and_then(move |(result, activated_job)| {
-//                let result: JobResult = result;
-//                let cloned_result_complete = result.clone();
-//                let cloned_result_fail = result.clone();
-//                let job_key = activated_job.key;
-//                let retries = activated_job.retries;
-//                match result {
-//                    JobResult::NoAction => {
-//                        JobResultFuture::NoAction(futures::future::ok((result, activated_job)))
-//                    }
-//                    JobResult::Fail { error_message } => {
-//                        let options = Default::default();
-//                        let mut fail_request = gateway::FailJobRequest::default();
-//                        fail_request.set_jobKey(job_key);
-//                        fail_request.set_retries(retries - 1);
-//                        if let Some(error_message) = error_message {
-//                            fail_request.set_errorMessage(error_message);
-//                        }
-//                        JobResultFuture::Fail(
-//                            gateway_client
-//                                .fail_job(options, fail_request)
-//                                .drop_metadata()
-//                                .map_err(|e| Error::FailJobError(e))
-//                                .map(move |_| (cloned_result_complete, activated_job)),
-//                        )
-//                    }
-//                    JobResult::Complete { variables } => {
-//                        let options = Default::default();
-//                        let mut complete_request = gateway::CompleteJobRequest::default();
-//                        complete_request.set_jobKey(job_key);
-//                        if let Some(variables) = variables {
-//                            complete_request.set_variables(variables)
-//                        }
-//                        JobResultFuture::Complete(
-//                            gateway_client
-//                                .complete_job(options, complete_request)
-//                                .drop_metadata()
-//                                .map_err(|e| Error::CompleteJobError(e))
-//                                .map(move |_| (cloned_result_fail, activated_job)),
-//                        )
-//                    }
-//                }
-
 #[runtime::main]
 async fn main() {
-    let mut client = Client::new("127.0.0.1", 26500).expect("Could not connect to broker.");
+    let client = Client::new("127.0.0.1", 26500).expect("Could not connect to broker.");
 
 //    let opt = Opt::from_args();
     let opt = Opt::ProcessJobs;
@@ -202,9 +92,6 @@ async fn main() {
         Opt::ProcessJobs => {
             let order_id_counter = Arc::new(RelaxedCounter::new(0));
 
-            let f = |aj: ActivatedJob| { async { JobResult::NoAction } };
-            let f = Arc::new(f);
-
             let initial_payment_config = WorkerConfig::new(
                 "rusty-worker".to_string(),
                 "initiate-payment".to_string(),
@@ -230,8 +117,26 @@ async fn main() {
                 }.boxed()
             };
 
+            let ship_without_insurance_config = WorkerConfig::new(
+                "rusty-worker".to_string(),
+                "ship-without-insurance".to_string(),
+                Duration::from_secs(3).as_millis() as _,
+                4,
+                PanicOption::FailJobOnPanic,
+            );
+
+            let ship_with_insurance_config = WorkerConfig::new(
+                "rusty-worker".to_string(),
+                "ship-with-insurance".to_string(),
+                Duration::from_secs(3).as_millis() as _,
+                4,
+                PanicOption::FailJobOnPanic,
+            );
+
             WorkerBuilder::new_with_interval_and_client(Interval::new(Duration::from_secs(5)), Arc::new(client))
                 .add_job_handler("initiate-payment", initial_payment_config, initial_payment_handler)
+                .add_job_handler("ship-without-insurance", ship_without_insurance_config, |_| async { JobResult::Complete { variables: None } }.boxed())
+                .add_job_handler("ship-with-insurance", ship_with_insurance_config, |_| async { JobResult::Complete { variables: None } }.boxed())
                 .into_future().await;
         }
     }
