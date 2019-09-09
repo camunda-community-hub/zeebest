a8bb07678285db0a8945cf1ce1906a70d5513a13
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2019-09-08 18:43:59 -0700

    remove a bunch of commented code

--- src/worker_builder.rs
+++ src/worker_builder.rs
@@ -16,6 +16,7 @@ impl<S: Stream + Unpin> WorkerBuilder<S> {
         self.handlers.insert(name,  (wc, Arc::new(f)));
         self
     }
+
     pub fn new_with_interval_and_client(interval: S, client: Arc<Client>) -> Self {
         WorkerBuilder {
             interval,
@@ -33,7 +34,6 @@ impl<S: Stream + Unpin> WorkerBuilder<S> {
                 async move {
                     let activate_jobs = ActivateJobs::new(wc.worker_name.clone(), wc.job_type.clone(), wc.timeout, wc.max_jobs_to_activate);
                     let mut activate_jobs_stream = client.activate_jobs(activate_jobs);
-
                     loop {
                         match activate_jobs_stream.next().await {
                             Some(Ok(j)) => {
@@ -43,7 +43,7 @@ impl<S: Stream + Unpin> WorkerBuilder<S> {
                                         let client = client.clone();
                                         let job_key: i64 = aj.key;
                                         let retries = aj.retries;
-                                        let panic_option = PanicOption::FailJobOnPanic;
+                                        let panic_option = wc.panic_option;
                                         match AssertUnwindSafe(f(aj)).catch_unwind().await {
                                             Ok(JobResult::NoAction) => {},
                                             Ok(JobResult::Complete {variables}) => {
@@ -76,107 +76,9 @@ impl<S: Stream + Unpin> WorkerBuilder<S> {
                             }
                         }
                     }
-
-//                    while let Some(Ok(j)) = activate_jobs_stream.next().await {
-//                        let it = j.activated_jobs.into_iter().map(|aj| {
-//                            async {
-//                                let f = f.clone();
-//                                let client = client.clone();
-//                                let job_key: i64 = aj.key;
-//                                let retries = aj.retries;
-//                                let panic_option = PanicOption::FailJobOnPanic;
-//                                match AssertUnwindSafe(f(aj)).catch_unwind().await {
-//                                    Ok(JobResult::NoAction) => {},
-//                                    Ok(JobResult::Complete {variables}) => {
-//                                        println!("complete job");
-//                                        let complete_job = CompleteJob { job_key, variables };
-//                                        client.complete_job(complete_job).await.unwrap();
-//                                    },
-//                                    Ok(JobResult::Fail {error_message}) => {
-//                                        client.fail_job(job_key, retries - 1).await.unwrap();
-//                                    }
-//                                    Err(_) => {
-//                                        match panic_option {
-//                                            PanicOption::DoNothingOnPanic => {
-//                                            },
-//                                            PanicOption::FailJobOnPanic => {
-//                                            }
-//                                        }
-//                                    },
-//                                };
-//                            }
-//                        });
-//                        futures::future::join_all(it).await;
-//                    }
                 }
             });
             futures::future::join_all(i).await;
         }
     }
 }
-
-//
-//impl<I, Fut1> WorkerBuilder<I, Fut1>
-//    where
-//        I: Stream + Unpin,
-//        Fut1: Future<Output = JobResult> + Send + UnwindSafe + 'static,
-//{
-////    pub fn with_interval(interval: I) -> Self {
-////        WorkerBuilder {
-////            interval,
-////            workers: HashMap::new(),
-////        }
-////    }
-//
-////    pub fn set_worker<F: Fn(ActivatedJob) -> Fut1 + Send + Sync + 'static>(mut self, wc: WorkerConfig, f: F) -> Self {
-////        let job_type = wc.job_type.clone();
-////        self.workers.insert(job_type, (wc, Arc::new(f)));
-////        self
-////    }
-//
-//    pub async fn build(self, client: Arc<Client>) -> () {
-//        let mut interval = self.interval;
-//        let job_handlers: Vec<(String, WorkerConfig, Arc<dyn Fn(ActivatedJob) -> Fut1 + Send + Sync>)> = self.workers.into_iter().map(|(jt,(wc, f))| (jt, wc, f)).collect();
-//        while let Some(_) = interval.next().await {
-//            let activate_jobs = ActivateJobs::new("the_worker", "the_job_type", 10000, 10);
-//            for jh in job_handlers.iter() {
-//                let (job_type, wc, f): &(String, WorkerConfig, Arc<dyn Fn(ActivatedJob) -> Fut1 + Send + Sync>) = jh;
-////                let client = self.client.clone().unwrap();
-//                runtime::spawn(async move {
-//                    let activate_jobs = ActivateJobs::new(wc.worker_name.clone(), job_type.to_string(), wc.timeout, wc.max_jobs_to_activate);
-//                    let panic_option = wc.panic_option;
-//                    let mut activate_jobs_stream = client.activate_jobs(activate_jobs);
-//                    while let Some(Ok(activated_jobs)) = activate_jobs_stream.next().await {
-//                        for aj in activated_jobs.activated_jobs.into_iter() {
-//                            let f = f.clone();
-//                            let klient = client.clone();
-//                            let job_key = aj.key;
-//                            let retries = aj.retries;
-//                            runtime::spawn(async move {
-//                                match f(aj).catch_unwind().await {
-//                                    Ok(JobResult::NoAction) => {},
-//                                    Ok(JobResult::Complete {variables}) => {
-//                                        let complete_job = CompleteJob { job_key, variables };
-//                                        klient.complete_job(complete_job).await.unwrap();
-//                                    },
-//                                    Ok(JobResult::Fail {error_message}) => {
-//                                        klient.fail_job(job_key, retries - 1).await.unwrap();
-//                                    }
-//                                    Err(_) => {
-//                                        match panic_option {
-//                                            PanicOption::DoNothingOnPanic => {
-//                                            },
-//                                            PanicOption::FailJobOnPanic => {
-//                                            }
-//                                        }
-//                                    },
-//                                };
-//                            });
-//                        };
-//                    }
-//                });
-//            }
-//        }
-//        ()
-//    }
-//}
